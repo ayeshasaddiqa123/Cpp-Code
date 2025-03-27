9150305b0d4fb6a0cb5839dbdafb2b4cd51f8ad9
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-03-27 11:50:33 +0500

    switch.cpp

---
+++ 01/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+#include<iostream>       // include the iostream library (a preprocessor directive)
+using namespace std;     // use the stanadard namespace
+int main ()               // main function from where execution starts
