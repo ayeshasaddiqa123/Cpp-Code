cb51728aa733841c7f720c2b7c3c137916823ebd
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-03-28 17:07:39 +0500

    digitsReverse.cpp

---
+++ 01/digitsReverse.cpp
@@ -0,0 +1,26 @@
+
+/*Write a program that inputs a six digit number and displays it in reverse order as 458762 to 267854.*/
+#include<iostream>       // include the iostream library (a preprocessor directive)
+using namespace std;     // use the stanadard namespace
+int main ()               // main function from where execution starts
+{
+    int n;
+    cout << "Enter six digits number="; //display message
+    cin>>n;                              //input
+    int a=n/100000;
+        n=n%100000;
+
+    int b=n/10000;
+        n=n%10000;
+
+    int c=n/1000;
+        n=n%1000;
+
+    int g=n/100;
+	    n=n%100;
+        
+	int h=n/10;
+	    n=n%10;	 
+    cout<<"In reverse order= "<<n<<h<<g<<c<<b<<a;
+	return 0;
+}
