c79b9701c58ebf8a2d66a7503297a4ee77bf6da4
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-04-19 08:35:01 +0500

    arrayInputAndOutput.cpp

--- 02/arrayInputAndOutput.cpp
+++ 02/arrayInputAndOutput.cpp
@@ -1,16 +1,16 @@
 // Aprogram that inputs values in array and then disspalys these values on screen
 
-#include<iostream>
-using namespace std;
-int main()
+#include<iostream>    //include the iostream library(a preprocessor directive)
+using namespace std;  //use standard namespace
+int main()            //main function from where execution start
 {
-	int arr[5];
+	int arr[5];                 //declare variable arr
 	    cout<<"Store values in array . "<<endl;
 	
 	for(int i=0;i<5;i++)
 	{
-		cout<<"Enter a number : ";
-		cin>>arr[i];
+		cout<<"Enter a number : ";       //prompt the user for input
+		cin>>arr[i];                    //cin object to input values
 	}
 	
 	    cout<<"The values stored in arrays are :- ";
@@ -18,8 +18,8 @@ int main()
 	for(int i=0;i<5;i++)
 	{
 		
-		cout<<arr[i]<<endl;
+		cout<<arr[i]<<endl;    // display values on screen
 	}
 	
-	
+	return 0; //return 0 to  the operating system
 }
