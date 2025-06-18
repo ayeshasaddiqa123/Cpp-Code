b4499e8db60e85381a65d238ffd48d1eda68f882
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-06-18 11:48:38 +0500

    pointers basics

---
+++ 02/pointers.cpp
@@ -0,0 +1,36 @@
+// program 1
+
+#include<iostream>     // include the iostream library (a preprocessor directive)
+using namespace std;     // use the stanadard namespace
+int main ()               // main function from where execution starts
+{
+	int *ptr;
+	int a;
+	ptr=&a;
+	cout<<"The address of  value stored is : "<<ptr;
+   return 0; //return 0 to operating system
+}
+
+
+
+// program 2
+
+#include<iostream>     // include the iostream library (a preprocessor directive)
+using namespace std;     // use the stanadard namespace
+int main ()               // main function from where execution starts
+{
+	int *ptr1,*ptr2;
+	int a;
+	int sum;
+	int b;
+	
+	
+	cout<<"Enter values of a and b :";
+	cin>>a>>b;
+	ptr1 = &a;
+	ptr2 = &b;
+	sum = *ptr1 + *ptr2;
+	
+	cout<<"The sum of  values stored is : " << sum;
+   return 0; //return 0 to operating system
+}
