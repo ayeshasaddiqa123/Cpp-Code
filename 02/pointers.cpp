4c3393910da1fdde5064b0e53365582b106a1ac8
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-06-20 14:34:06 +0500

    pointers

--- 02/pointers.cpp
+++ 02/pointers.cpp
@@ -34,3 +34,29 @@ int main ()               // main function from where execution starts
 	cout<<"The sum of  values stored is : " << sum;
    return 0; //return 0 to operating system
 }
+
+
+
+
+
+// program 3
+
+#include<iostream>     // include the iostream library (a preprocessor directive)
+using namespace std;     // use the stanadard namespace
+int main ()               // main function from where execution starts
+{
+	int marks[5];
+	int *ptr;
+	cout<<"Enter five marks : ";
+	for(int i=0;i<5;i++) 
+		cin>>marks[i];
+		
+		ptr=marks;
+		cout<<"You entered the following values :";\
+		for(int i=0;i<5;i++)
+		cout <<*ptr++<<" ";
+		
+
+
+   return 0; //return 0 to operating system
+}
